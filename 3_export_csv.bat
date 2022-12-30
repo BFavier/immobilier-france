242d6cba5f8780153a909c8140a502046ece10ec
BenoitFavier <bade@webmails.top>
2022-12-30 15:18:01 +0100

    renamed scripts

--- 3_export_csv.bat
+++
@@ -1,8 +0,0 @@
-SETLOCAL ENABLEDELAYEDEXPANSION
-SET PGPASSWORD=password
-SET MYPATH=%~dp0
-Rem give full control to the folder to all users
-icacls "!MYPATH!tables" /grant "*S-1-1-0":(OI)(CI)F
-FOR %%S IN (dvf_d69) DO (
-	 psql -U postgres -d dvf -f "!MYPATH!export.sql" -v schema="%%S" -v export_path="'!MYPATH!tables\export.csv'"
-)
