239d3aa4bcbd3bcb33260a070950aa87dd5ccdd7
BenoitFavier <bade@webmails.top>
2022-12-30 15:17:01 +0100

    modified export script

--- export.sql
+++ export.sql
@@ -1,14 +1,16 @@
 CREATE LOCAL TEMPORARY VIEW valid_mutations AS
 (
-    SELECT idmutation, vefa, datemut AS "date", valeurfonc AS "price"
-    FROM dvf_d69.mutation
-    WHERE (nblocmai + nblocapt <= 1)
+    SELECT idmutation, vefa, datemut AS "transaction_date", valeurfonc AS "price"
+    FROM :schema.mutation
+    WHERE (nblocmai + nblocapt = 1)
 );
 
 CREATE LOCAL TEMPORARY VIEW adresse AS
 (
-    SELECT idadresse, (novoie || btq || " " || typvoie || " " || voie) AS "addresse", commune AS "city", codepostal AS "zip_code"
-    FROM dvf_d69.adresse
+    SELECT l.idmutation, a.commune AS "city", a.codepostal AS "zip_code",
+        CONCAT(a.novoie, a.btq, ' ', a.typvoie, ' ', a.voie) AS "address"
+    FROM :schema.adresse AS a, :schema.adresse_local AS al, :schema.local AS l
+    WHERE (a.idadresse = al.idadresse) AND (al.iddispoloc = l.iddispoloc) AND (l.codtyploc <= 2)
 );
 
 CREATE LOCAL TEMPORARY VIEW habitations AS
@@ -18,49 +20,64 @@ CREATE LOCAL TEMPORARY VIEW habitations AS
         nbpprinc AS "n_rooms",
         sbati AS "housing_surface",
         ST_AsText(geomloc) AS "coordinates"
-    FROM dvf_d69.local
+    FROM :schema.local
     WHERE (codtyploc <=2) AND (idmutation IN (SELECT idmutation FROM valid_mutations))
 );
 
 CREATE LOCAL TEMPORARY VIEW dependances AS
 (
-    SELECT MAX(idmutation), array_agg(sbati) AS "annexe_surfaces"
-    FROM dvf_d69.local
+    SELECT MAX(idmutation) AS "idmutation", array_agg(sbati) AS "annexe_surfaces"
+    FROM :schema.local
     WHERE (codtyploc = 3) AND (idmutation IN (SELECT idmutation FROM valid_mutations))
     GROUP BY idmutation
 );
 
 CREATE LOCAL TEMPORARY VIEW locaux_commerciaux AS
 (
-    SELECT idmutation, array_agg(sbati) AS "commercial_lot_surfaces"
-    FROM dvf_d69.local
+    SELECT MAX(idmutation) AS "idmutation", array_agg(sbati) AS "commercial_lot_surfaces"
+    FROM :schema.local
     WHERE (codtyploc = 4) AND (idmutation IN (SELECT idmutation FROM valid_mutations))
     GROUP BY idmutation
 );
 
 CREATE LOCAL TEMPORARY VIEW parcelles AS
 (
-    SELECT idmutation,
+    SELECT MAX(idmutation) AS "idmutation",
         array_agg(dcntagri) AS "field_surfaces",
         array_agg(dcntsol) AS "ground_surfaces",
         array_agg(dcntnat) AS "nature_surfaces"
-    FROM dvf_d69.disposition_parcelle
+    FROM :schema.disposition_parcelle
     WHERE (parcvendue) AND (idmutation IN (SELECT idmutation FROM valid_mutations))
     GROUP BY idmutation
 );
 
 CREATE LOCAL TEMPORARY VIEW carrez AS
 (
-    SELECT idmutation, SUM(scarrez) AS "carrez_surface"
-    FROM dvf_d69.lot
+    SELECT MAX(idmutation) AS "idmutation", array_agg(scarrez) AS "carrez_surfaces"
+    FROM :schema.lot
     WHERE idmutation IN (SELECT idmutation FROM valid_mutations)
     GROUP BY idmutation
-)
+);
 
--- COPY
--- (
---     SELECT * FROM valid_mutations
---     JOIN loc ON loc.idloc IN (SELECT idloc FROM valid_mutations)
--- )
--- TO 'C:\Users\Benoit\Documents\Project\DVFplus\tables\export.csv' (DELIMITER ',', FORMAT CSV, ENCODING 'UTF-8', HEADER)
--- ;
+CREATE LOCAL TEMPORARY VIEW joined AS
+(
+    SELECT vm.transaction_date, vm.price, a.city, a.zip_code, a.address,
+        h.housing_type, h.n_rooms, h.housing_surface, h.coordinates,
+        d.annexe_surfaces, lc.commercial_lot_surfaces, p.field_surfaces,
+        p.ground_surfaces, p.nature_surfaces, c.carrez_surfaces
+    FROM valid_mutations AS vm
+    LEFT JOIN adresse AS a
+    ON vm.idmutation = a.idmutation
+    LEFT JOIN habitations AS h
+    ON vm.idmutation = h.idmutation
+    LEFT JOIN dependances AS d
+    ON vm.idmutation = d.idmutation
+    LEFT JOIN locaux_commerciaux AS lc
+    ON vm.idmutation = lc.idmutation
+    LEFT JOIN parcelles AS p
+    ON vm.idmutation = p.idmutation
+    LEFT JOIN carrez AS c
+    ON vm.idmutation = c.idmutation
+);
+
+COPY (SELECT * FROM joined) TO :export_path (DELIMITER ',', FORMAT CSV, ENCODING 'UTF-8', HEADER);
